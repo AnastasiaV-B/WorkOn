c470cc40074e58b4c674457907a222b05da8bcf5
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-13 23:36:07 +0300

    работа онлайн

--- Work01/Program.cs
+++ Work01/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿// Задача 45: Напишите программу,
+// которая будет создавать копию заданного 
+// массива с помощью поэлементного копирования.
+
+// 2. создать массив
+int[] FirstArray(int size)
+{
+    return new int[10];
+}
+
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+// 3. печать массива
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+int []arr=FirstArray(10);
+FillArray(arr);
+PrintArray(arr);

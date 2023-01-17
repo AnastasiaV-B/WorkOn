dcc113adecc3469ee663f1101bb578ba24a45eaf
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-17 12:01:17 +0300

    работа онлайн 5

--- W05/Program.cs
+++ W05/Program.cs
@@ -1,10 +1,10 @@
-﻿//  Код для программы сложения двух чисел.
+﻿// Программа для вычисления частного (/) двух чисел.
 
 // int - ввод для целочисленных данных, а 
 // double - ввод вещественных или дробных чисел.
 // result - промежуточная переменная вывода.
 
-int numberA = 31;
-int numberB = 5;
-int result = numberA + numberB;
-Console.WriteLine(result);
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
+

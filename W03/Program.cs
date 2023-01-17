6215dd6ce4e8bc6fd7453a2bcba7b404ab293733
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-17 14:14:28 +0300

    работа онлайн 7

--- W03/Program.cs
+++ W03/Program.cs
@@ -3,6 +3,8 @@
 // Если писать код с Console.Write() ,то программа выведет все на одной строке...
 
 Console.WriteLine("Введите ваше имя ");
+
 string username = Console.ReadLine();
+
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
---
+++ W07/Program.cs
@@ -0,0 +1,25 @@
+﻿// Код для персонального приветствия. (метод ветвления)
+
+// if(оператор условия)
+// {
+//   набор действий 1
+// }
+// else 
+// {
+//  набор действий 2
+// }
+
+// ToLover() - переводит все символы в нижний регистр.
+
+Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

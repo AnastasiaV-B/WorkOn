5f6e48e635f8fa3589e8738b946d39fc9d90d510
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-17 20:36:38 +0300

    работа онлайн 10

--- W10/Program.cs
+++ W10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Палиндром.
+
+bool Check(int value)
+{
+  int t = value;
+  int inverseValue = 0;
+
+  while (t != 0)
+  {
+    int o = t % 10;
+    inverseValue = inverseValue * 10 + o;
+    t = t / 10;
+  }
+  return value == inverseValue;
+}
+
+Console.WriteLine(Check(1223221));

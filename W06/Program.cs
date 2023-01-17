20607583faafd400d0975056a6fa80c9d36ad55b
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-17 13:46:44 +0300

    работа онлайн 6

---
+++ W06/Program.cs
@@ -0,0 +1,13 @@
+﻿// Генератор псевдослучайных чисел. Пример вычисления суммы.
+
+// Random - ввод случайных (сгенерированных) чисел.
+
+// new Random().Next(min, max) - даст случайное целое число от min до max-1,
+//                                              [min, max-1] или [min, max)
+
+int numberA = new Random().Next(1, 10); // 1, 2, 3, 4, ...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);

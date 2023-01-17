63a2940fdb4b84b49af1d2496d84a80907f5bb5b
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-17 11:48:30 +0300

    работа онлайн 4

---
+++ W04/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Код для программы сложения двух чисел.
+
+// int - ввод для целочисленных данных, а 
+// doubl - ввод вещественных или дробных чисел.
+// result - промежуточная переменная вывода.
+
+int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+

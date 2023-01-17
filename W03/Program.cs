3b7ef5fd342d40bdbe726b782edfe748a6b6a178
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-17 11:23:03 +0300

    работа онлайн 3

---
+++ W03/Program.cs
@@ -0,0 +1,8 @@
+﻿//  Код для программы запроса данных у пользователя.
+
+// Если писать код с Console.Write() ,то программа выведет все на одной строке...
+
+Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
